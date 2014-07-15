44ba97123e0f3e896ee064cf19e1c020e1e0c6e9
James Adam <james@lazyatom.com>
2014-07-14 23:16:20 -0500

    Don't show lambdas in the env when debugging

--- lib/debug.rb
+++ lib/debug.rb
@@ -1,5 +1,5 @@
 $debug_level = 0
-$debug = ENV['DEBUG']
+$debug = ENV['DEBUG'] #|| true
 
 def debug_print(level, thing)
   return unless $debug
@@ -9,21 +9,13 @@ def debug_print(level, thing)
 end
 
 def debug_environment_ignore_keys
-  LittleScheme::Evaluator.new.default_environment.keys + [
-    :>,
-    :<,
-    :'=',
-    :eqan?,
-    :eqlist?,
-    :equal?,
-    :'rember-f',
-  ]
+  LittleScheme::Evaluator.new.default_environment.keys
 end
 
 def debug(values={}, &block)
   if $debug
     if values[:env]
-      values[:env] = values[:env].dup.delete_if { |k,_| debug_environment_ignore_keys.include?(k) }
+      values[:env] = values[:env].dup.delete_if { |k,v| debug_environment_ignore_keys.include?(k) || v.is_a?(LittleScheme::Evaluator::Lambda::Compiled) }
     end
     if block_given?
       debug_print $debug_level, values
